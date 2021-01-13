cceed193fcd1e8e58600191255abf62c50f63129
wanghongbo02 <wanghongbo02@sunlands.com>
2021-01-14 02:02:07 +0800

    基础入库版

--- python/reptile/cartoon/getInfo.py
+++ python/reptile/cartoon/getInfo.py
@@ -53,19 +53,63 @@ def getRemarkInfo(html,title):
 
 def saveInfo(listTitle, listRemark, title,url,conn):
     cursor = conn.cursor()
-    # sql = "INSERT INTO cartoon_info(area,type,issue,language,title,alias,label,introduction,remark,poster_url,link_url) VALUES (%s, %s,%s, %s,%s, %s,%s, %s,%s, %s);"
-    sql = "INSERT INTO cartoon_info(area,type,issue) VALUES (%s, %s,%s);"
+    sql = "INSERT INTO cartoon_info(area,type,issue,language,title,alias,label,introduction,remark,poster_url,link_url) VALUES (%s, %s,%s, %s,%s, %s,%s, %s, %s,%s, %s);"
     try:
         # 执行SQL语句
-        # cursor.execute(sql, [listTitle[1],listTitle[2],listTitle[3],'日漫',title,listTitle[0],listTitle[5],listRemark[0],listTitle[6],listRemark[1],url])
-        cursor.execute(sql, ['1','2','2021'])
+        cursor.execute(sql, [listTitle[1],listTitle[2],listTitle[3],'日语动漫',title,listTitle[0],listTitle[5],listRemark[0],listTitle[6],listRemark[1],url])
         # 提交事务
         conn.commit()
     except Exception as e:
         # 有异常，回滚事务
         conn.rollback()
     cursor.close()
-    # conn.close()
+
+
+def savePlayer(palyUrl,title,i,conn):
+    i -=1
+    tar = 'http://www.imomoe.ai'
+    target = tar + palyUrl
+    req = requests.get(url = target)
+    req.encoding = req.apparent_encoding
+    html = req.text
+    # print(html)
+    div_bf = BeautifulSoup(html,features = "lxml")
+    div = div_bf.find_all('div', class_ = 'player')
+    # print(div)
+    script_bf = BeautifulSoup(str(div[0]),features = "lxml")
+    a = script_bf.find_all('script')
+    path = ''
+    for eacha in a:
+        if(eacha.get('src') is not None):
+            path = eacha.get('src')
+    path = tar + path
+    # print(path)
+    req = requests.get(url = path)
+    req.encoding = req.apparent_encoding
+    mp4Json = req.text
+    # print(mp4Json)
+    orderId =1
+    type =1
+    list = mp4Json.split('$')
+    print(palyUrl)
+    cursor = conn.cursor()
+    for each in list:
+        if(each.find('http') == 0):
+            print(each)
+            if(orderId > i):
+                type += 1
+                orderId =1
+            sql = "INSERT INTO cartoon_play(link_url,title,order_id,type) VALUES (%s,%s,%s,%s);"
+            try:
+                # 执行SQL语句
+                cursor.execute(sql, [each,title,orderId,type])
+                orderId += 1
+                # 提交事务
+                conn.commit()
+            except Exception as e:
+                # 有异常，回滚事务
+                conn.rollback()
+    cursor.close()
 
 
 def getCartoonInfo(eacha,conn):
@@ -88,8 +132,25 @@ def getCartoonInfo(eacha,conn):
     a_bf = BeautifulSoup(str(div[0]),features = "lxml")
     # print(a_bf)
     a = a_bf.find_all('a')
+    i =1
+    palyUrl = ''
+    cursor = conn.cursor()
     for eacha in a:
         print(eacha.string, eacha.get('href'))
+        if (i == 1):
+            palyUrl = eacha.get('href')
+        sql = "INSERT INTO cartoon_file(link_url,title,order_id,order_info) VALUES (%s,%s,%s,%s);"
+        try:
+            # 执行SQL语句
+            cursor.execute(sql, [eacha.get('href'),title,i,eacha.string])
+            i+=1
+            # 提交事务
+            conn.commit()
+        except Exception as e:
+            # 有异常，回滚事务
+            conn.rollback()
+    cursor.close()
+    savePlayer(palyUrl,title,i,conn)
 
 
 
@@ -137,10 +198,11 @@ def print_page_info(path,conn):
     return resut
 
 if __name__ == "__main__":
-    conn = pymysql.connect(host='127.0.0.1', user='root',password='wanghongbo',database='aohayou',charset='utf8')
     href = '?page=1&searchword=%C8%D5%D3%EF&searchtype=-1'
     while True:
+        conn = pymysql.connect(host='127.0.0.1', user='root',password='wanghongbo',database='aohayou',charset='utf8')
         resut = print_page_info(href,conn)
+        conn.close()
         if(resut == ''):
             break
         href = resut
